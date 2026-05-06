69c5368e6877b450f208c8af3e5188dec04ee3fb
amatsagu <81469656+amatsagu@users.noreply.github.com>
2026-05-06 21:10:35 +0200

    feat: reset entry file

--- main.go
+++ main.go
@@ -7,31 +7,14 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run . <filename>")
+		fmt.Println("Usage: stelar <filename>")
 		return
 	}
 
-	filename := os.Args[1]
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
-		return
-	}
+	// content, err := os.ReadFile(os.Args[1])
+	// if err != nil {
+	// 	fmt.Printf("Error: %v\n", err)
+	// 	return
+	// }
 
-	l := NewLexer(string(content))
-
-	for {
-		tok := l.NextToken()
-		if tok.Type == EOF_TOKEN {
-			break
-		}
-
-		fmt.Printf("%s:%d:%d Type: %v, Literal: %q\n",
-			filename,
-			tok.Line,
-			tok.Column,
-			tok.Type,
-			tok.Literal,
-		)
-	}
 }
